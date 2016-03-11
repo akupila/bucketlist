17bb48932bf5a96111a58b62b9382f4d5f962dbf
akupila <akupila@gmail.com>
2016-03-11 11:23:13 +0100

    Disable output in CI and test, closes #3

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ module.exports = function (taskList, config) {
     showLogs: true
   })
 
+  const disableOutput = process.env.CI || process.env.NODE_ENV === 'test'
+
   // -----------------------------------------------------
 
   const states = {
@@ -108,33 +110,41 @@ module.exports = function (taskList, config) {
       }
     })
 
-  const charm = makeCharm(process)
-  charm.on('^C', process.exit)
+  let charm
+  if (!disableOutput) {
+    charm = makeCharm(process)
+    charm.on('^C', process.exit)
+  }
 
   return Rx.Observable.create((observer) => {
+    if (disableOutput) {
+      observer.next({x: 0, y: 0})
+      observer.complete()
+      return
+    }
+
     charm.position((x, y) => {
       // Make some space if we've scrolled all the way down
       // If we don't do this the terminal will go crazy and scroll
       // for every render
       const missingRows = Math.max(y + flatTasks.length - process.stdout.rows, 0)
-      for (let i = 0; i < missingRows; i++) {
-        charm.write('\n')
-      }
-
+      charm.write(pad(missingRows, '', { char: '\n' }))
       observer.next({x, y: y - missingRows})
       observer.complete()
     })
   }).flatMap((initialCursor) => {
-    charm.cursor(false)
-
     function resetCursor () {
+      if (disableOutput) return
       charm.display('reset')
       charm.cursor(true)
       charm.position(initialCursor.x, initialCursor.y + flatTasks.length)
       charm.destroy()
     }
 
-    process.on('exit', resetCursor)
+    if (!disableOutput) {
+      charm.cursor(false)
+      process.on('exit', resetCursor)
+    }
 
     return Rx.Observable.concat(
       // Empty list first
@@ -144,6 +154,14 @@ module.exports = function (taskList, config) {
     // debounce? bufferWithTime?
     .do((task) => {
       if (task instanceof Error) return
+
+      if (disableOutput) {
+        if (task.state === states.COMPLETED && process.env.NODE_ENV !== 'test') {
+          console.log(task.name + ' completed')
+        }
+        return
+      }
+
       // Render
       charm.position(0, initialCursor.y + task.index)
       charm.erase('line')
